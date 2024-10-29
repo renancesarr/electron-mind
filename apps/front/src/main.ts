49ffdf2338a81db3a94b9223e9d94b467c81745c
Renan Rabelo <renancesarr@users.noreply.github.com>
2024-10-29 02:16:56 +0000

    app iniciando já via container, com tela branca

--- apps/front/src/main.ts
+++ apps/front/src/main.ts
@@ -1,30 +1,33 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
+app.commandLine.appendSwitch('no-sandbox');
+app.commandLine.appendSwitch('disable-gpu-sandbox');
+
+
 const createWindow = () => {
+
+
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
-  // and load the index.html of the app.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
-  }
+  mainWindow.loadFile(path.join(__dirname, '../../index.html'));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+
 };
 
 // This method will be called when Electron has finished
